a1a2366ff654ad87e51345262c9289ec957ef7dd
Chris Jones <cjone141@tnstate.edu>
2023-09-28 14:06:58 -0500

    changed README to reflect BST and double linked list, started to code BST

---
+++ BinarySearchTree/BSTDriver.java
@@ -0,0 +1,9 @@
+package BinarySearchTree;
+
+public class BSTDriver {
+    public static void main(String[] args){
+        Node test1 = new Node();
+        Node test2 = new Node(15);
+    }
+    
+}
---
+++ BinarySearchTree/Node.java
@@ -0,0 +1,20 @@
+package BinarySearchTree;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(){
+        data = 0;
+        left = null;
+        right = null;
+    }
+
+    public Node(int d){
+        data = d;
+        left = null;
+        right = null;
+    }
+    
+}
