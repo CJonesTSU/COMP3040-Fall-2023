96f738cf19ddf82b4f482e202d6450de3657e1b5
Chris Jones <cjone141@tnstate.edu>
2023-11-07 06:45:14 -0600

    Added Heap, driver code still very old school (pre-scanner)

---
+++ Heap/Node.java
@@ -0,0 +1,21 @@
+package Heap;
+
+public class Node {
+    private int data; // data item (key)
+    // -------------------------------------------------------------
+    public Node(int key) // constructor
+    {
+        data = key;
+    }
+    // -------------------------------------------------------------
+    public int getKey()
+    {
+        return data;
+    }
+    // -------------------------------------------------------------
+    public void setKey(int id)
+    {
+        data = id;
+    }
+    // -------------------------------------------------------------
+} // end class Node
